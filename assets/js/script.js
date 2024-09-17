83f53881c8345b4a3989d8106904c3f6b13981da
Jack Pitz <jackpitz95@gmail.com>
2024-09-17 14:08:00 +0000

    Add questions for quiz in js file

---
+++ assets/js/script.js
@@ -0,0 +1,118 @@
+const questions = [
+    {
+        question: "What is the capital of Ontario ?",
+        optionA: "Hamilton"
+        optionB: "Ottawa"
+        optionC: "Toronto"
+        correctOption: "optionC"
+
+    },
+
+    {
+        question: "What is the capital of Quebec ?",
+        optionA: "Montreal"'
+        optionB: "Quebec City"
+        optionC: "Gatineau"
+       correctOption: "optionB"
+    },
+
+    {
+        question: "What is the capital of British Columbia ?"
+        optionA: "Victoria"
+        optionB: "Vancouver"
+        optionC: "Surrey"
+        correctOption: "optionA"
+
+    },
+
+    {
+        question: "What is the capital of Nova Scotia ?"
+        optionA: "Cape Breton"
+        optionB: "Halifax"
+        optionC: "Lunenburg"
+        correctOption: "optionB"
+
+    },
+
+    {
+        question: "What is the capital of Manitoba ?"
+        optionA: "Brandon"
+        optionB: "Winkler"
+        optionC: "Winnipeg"
+        correctOption: "optionC"
+
+    },
+
+    {
+        question: "What is the capital of Saskatchewan ?"
+        optionA: "Regina"
+        optionB: "Moose Jaw"
+        optionC: "Saskatoon"
+        correctOption: "optionA"
+
+    },
+
+    {
+        question: "What is the capital of Prince Edward Island ?"
+        optionA: "Charlottetown"
+        optionB: "Cornwall"
+        optionC: "Stratford"
+        correctOption: "optionA"
+
+    },
+
+    {
+        question: "What is the capital of Nunavut ?"
+        optionA: "Cambridge Bay"
+        optionB: "Iqluit"
+        optionC: "Arviat"
+        correctOption: "optionB"
+
+    },
+
+    {
+        question: "What is the capital of Yukon ?"
+        optionA: "Whitehorse"
+        optionB: "Faro"
+        optionC: "Dawson City"
+        correctOption: "optionA"
+
+    },
+
+    {
+        question: "What is the capital of Newfoundland and Labrador ?"
+        optionA: "Bay Roberts"
+        optionB: "Grand Falls-Windsor"
+        optionC: "St.John's"
+        correctOption: "optionC"
+
+    },
+
+    {
+        question: "What is the capital of Alberta ?"
+        optionA: "Calgary"
+        optionB: "Edmonton"
+        optionC: "Red Deer"
+        correctOption: "optionB"
+
+    },
+
+    {
+        question: "What is the capital of New Brunswick ?"
+        optionA: "Fredericton"
+        optionB: "Edmundston"
+        optionC: "Bathurst"
+        correctOption: "optionA"
+
+    },
+
+    {
+        question: "What is the capital of Northwest Terrirtories ?"
+        optionA: "Fort Smith"
+        optionB: "Inuvik"
+        optionC: "YellowKnife"
+        correctOption: "optionC"
+
+    },
+
+ ];
