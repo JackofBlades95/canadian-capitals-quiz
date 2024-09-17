d5f0eb259eee741d5e3babc454af4bf6913cd8a7
Jack Pitz <jackpitz95@gmail.com>
2024-09-17 15:27:19 +0000

    Add score and question variables to js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -122,3 +122,8 @@ const questionNumberElement = document.getElementById('question-number')
 const questionElement = document.getElementById('question')
 const buttonElement = document.getElementById('buttons')
 const nextButtonElement = document.getElementById('next-btn')
+
+let score = 0
+let questionNumber = 0
+
+
