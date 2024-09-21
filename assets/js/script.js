5ff83fdddad4067f65df273da156d21fb057ae31
Jack Pitz <jackpitz95@gmail.com>
2024-09-21 16:49:10 +0000

    Add questionindex and score variables in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,122 +1,122 @@
 const questions = [{
         question: "What is the capital of Ontario ?",
-        optionA: "Ottawa"
-        optionB: "Hamilton"
-        optionC: "Toronto"
+        optionA: "Ottawa",
+        optionB: "Hamilton",
+        optionC: "Toronto",
         correctOption: "optionC"
 
     },
 
     {
         question: "What is the capital of Quebec ?",
-        optionA: "Montreal"
-        optionB: "Quebec City"
-        optionC: "Gatineau"
+        optionA: "Montreal",
+        optionB: "Quebec City",
+        optionC: "Gatineau",
         correctOption: "optionB"
     },
 
     {
-        question: "What is the capital of British Columbia ?"
-        optionA: "Victoria"
-        optionB: "Vancouver"
-        optionC: "Surrey"
+        question: "What is the capital of British Columbia ?",
+        optionA: "Victoria",
+        optionB: "Vancouver",
+        optionC: "Surrey",
         correctOption: "optionA"
 
     },
 
     {
-        question: "What is the capital of Nova Scotia ?"
-        optionA: "Cape Breton"
-        optionB: "Halifax"
-        optionC: "Lunenburg"
+        question: "What is the capital of Nova Scotia ?",
+        optionA: "Cape Breton",
+        optionB: "Halifax",
+        optionC: "Lunenburg",
         correctOption: "optionB"
 
     },
 
     {
-        question: "What is the capital of Manitoba ?"
-        optionA: "Brandon"
-        optionB: "Winkler"
-        optionC: "Winnipeg"
+        question: "What is the capital of Manitoba ?",
+        optionA: "Brandon",
+        optionB: "Winkler",
+        optionC: "Winnipeg",
         correctOption: "optionC"
 
     },
 
     {
-        question: "What is the capital of Saskatchewan ?"
-        optionA: "Regina"
-        optionB: "Moose Jaw"
-        optionC: "Saskatoon"
+        question: "What is the capital of Saskatchewan ?",
+        optionA: "Regina",
+        optionB: "Moose Jaw",
+        optionC: "Saskatoon",
         correctOption: "optionA"
 
     },
 
     {
-        question: "What is the capital of Prince Edward Island ?"
-        optionA: "Charlottetown"
-        optionB: "Cornwall"
-        optionC: "Stratford"
+        question: "What is the capital of Prince Edward Island ?",
+        optionA: "Charlottetown",
+        optionB: "Cornwall",
+        optionC: "Stratford",
         correctOption: "optionA"
 
     },
 
     {
-        question: "What is the capital of Nunavut ?"
-        optionA: "Cambridge Bay"
-        optionB: "Iqluit"
-        optionC: "Arviat"
+        question: "What is the capital of Nunavut ?",
+        optionA: "Cambridge Bay",
+        optionB: "Iqluit",
+        optionC: "Arviat",
         correctOption: "optionB"
 
     },
 
     {
-        question: "What is the capital of Yukon ?"
-        optionA: "Whitehorse"
-        optionB: "Faro"
-        optionC: "Dawson City"
+        question: "What is the capital of Yukon ?",
+        optionA: "Whitehorse",
+        optionB: "Faro",
+        optionC: "Dawson City",
         correctOption: "optionA"
 
     },
 
     {
-        question: "What is the capital of Newfoundland and Labrador ?"
-        optionA: "Bay Roberts"
-        optionB: "Grand Falls-Windsor"
-        optionC: "St.John's"
+        question: "What is the capital of Newfoundland and Labrador ?",
+        optionA: "Bay Roberts",
+        optionB: "Grand Falls-Windsor",
+        optionC: "St.John's",
         correctOption: "optionC"
 
     },
 
     {
-        question: "What is the capital of Alberta ?"
-        optionA: "Calgary"
-        optionB: "Edmonton"
-        optionC: "Red Deer"
+        question: "What is the capital of Alberta ?",
+        optionA: "Calgary",
+        optionB: "Edmonton",
+        optionC: "Red Deer",
         correctOption: "optionB"
 
     },
 
     {
-        question: "What is the capital of New Brunswick ?"
-        optionA: "Fredericton"
-        optionB: "Edmundston"
-        optionC: "Bathurst"
+        question: "What is the capital of New Brunswick ?",
+        optionA: "Fredericton",
+        optionB: "Edmundston",
+        optionC: "Bathurst",
         correctOption: "optionA"
 
     },
 
     {
-        question: "What is the capital of Northwest Terrirtories ?"
-        optionA: "Fort Smith"
-        optionB: "Inuvik"
-        optionC: "YellowKnife"
+        question: "What is the capital of Northwest Terrirtories ?",
+        optionA: "Fort Smith",
+        optionB: "Inuvik",
+        optionC: "YellowKnife",
         correctOption: "optionC"
 
     },
 
 ];
 
-const quizContainer = document.getElementById('quiz')
+
 const scoreElement = document.getElementById('score')
 const questionNumberElement = document.getElementById('question-number')
 const questionElement = document.getElementById('question')
@@ -125,5 +125,3 @@ const nextButtonElement = document.getElementById('next-btn')
 
 let score = 0
 let questionNumber = 0
-
-
